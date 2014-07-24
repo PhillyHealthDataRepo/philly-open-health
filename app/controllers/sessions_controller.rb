c4a0a0329705069fe5383819869ae1566b4386d7
Gelsey Torres <gelsey.torres@gmail.com>
2014-07-23 23:12:09 -0400

    Change login to signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
-      login @user
+      signin @user
       redirect_to root_url, notice: "Signed in!"
     else
       flash.now.alert = "Email or password is invalid."
