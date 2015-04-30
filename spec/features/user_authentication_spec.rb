c6738aafbc335e98aa5434810c40b140993c3810
Ryan Moore <moorer@udel.edu>
2015-04-30 11:26:23 -0400

    Add context to User auth spec
    Fix formatting
    
    Add context to User auth spec

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -2,25 +2,58 @@ require "rails_helper"
 
 feature "User authentication" do
   let(:user) { create :user }
-  let(:sign_in_conf) { "Signed in as #{user.email}" } 
+  let(:sign_in_conf) { "Signed in as #{user.email}" }
+  let(:invalid_usr_msg) { "Email or password is invalid" }
 
   before :each do
     visit root_path
-
-    sign_in user
-  end
-  
-  scenario "User signs in" do
-    expect(page).to have_content sign_in_conf
   end
 
-  scenario "User signs out" do
-    sign_out
+  context "A User with no account" do
+    scenario "User tries to sign in" do
+      fake_user = build :user
 
-    expect(page).not_to have_content sign_in_conf
+      sign_in fake_user
+
+      expect(page).to have_content invalid_usr_msg
+    end
   end
 
-  def sign_in user
+  context "A valid User who types incorrect info" do
+    scenario "With improper password" do
+      user.password = "wrong password"
+
+      sign_in user
+
+      expect(page).to have_content invalid_usr_msg
+    end
+
+    scenario "With invalid email" do
+      user.email = "wrong@email.com"
+
+      sign_in user
+
+      expect(page).to have_content invalid_usr_msg
+    end
+  end
+
+  context "A User with a valid account" do
+    scenario "User signs in" do
+      sign_in user
+
+      expect(page).to have_content sign_in_conf
+    end
+
+    scenario "User signs out" do
+      sign_in user
+
+      sign_out
+
+      expect(page).not_to have_content sign_in_conf
+    end
+  end
+
+  def sign_in(user)
     click_link "Sign In"
 
     fill_in :email, with: user.email
@@ -33,4 +66,3 @@ feature "User authentication" do
     click_link "Sign Out"
   end
 end
-
